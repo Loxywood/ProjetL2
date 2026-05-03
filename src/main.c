2a3e6e239582348f0eed6910b841cb33d4134c01
Loxywood <yamini.bourhane@yahoo.com>
2026-05-03 14:44:19 +0200

    [Loxywood] - Ajout d'une petite musique d'ambiance

--- src/main.c
+++ src/main.c
@@ -6,14 +6,25 @@ int main(void)
 {
     //Ouverture de la fenêtre :
     InitWindow(960, 540, "Projet L2 info");
-    SetTargetFPS(60);
-    
+    InitAudioDevice(); 
+
+    float volume = 0.05f; //Valeur temporaire pour le bien de mon ouïe.
+    Music music = LoadMusicStream("assets/atlasaudio-ambient-cinematic-510518.mp3");
     Game game;
+
+    SetTargetFPS(60);
+    SetMusicVolume(music, volume);
+
+    PlayMusicStream(music);
+    
     InitGame(&game);
+    
 
     //Game loop.
     while (!WindowShouldClose())
     {
+        
+        UpdateMusicStream(music);
         //UpdateGame(&game);
         
         BeginDrawing();         
